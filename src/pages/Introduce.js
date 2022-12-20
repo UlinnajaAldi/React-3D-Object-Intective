ba8c7c202d0b7f6103f577857206125b40a8c0ee
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-20 17:35:45 +0700

    feat: add testing 3d to  components

--- src/pages/Introduce.js
+++ src/pages/Introduce.js
@@ -1,7 +1,12 @@
+import React from "react";
+import Toner from "../components/Toner";
+
 export const Introduce = () => {
   return (
-    <div>
-      <h1>Introduce</h1>
-    </div>
+    <>
+      <div className="flex items-center justify-center">
+        <Toner />
+      </div>
+    </>
   );
 };
