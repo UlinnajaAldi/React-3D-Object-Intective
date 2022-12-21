22ae4d1ba7cc1fa31cae80f2c1a6724b63eeabf6
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-21 19:22:54 +0700

    feat: update pura test 3d component

--- src/components/Toner.js
+++ src/components/Toner.js
@@ -2,6 +2,6 @@ import Spline from "@splinetool/react-spline";
 
 export default function App() {
   return (
-    <Spline scene="https://prod.spline.design/QMYHOhEvRFXgHI2P/scene.splinecode" />
+    <Spline scene="https://prod.spline.design/M4U6iHHuCfTMVjsh/scene.splinecode" />
   );
 }
