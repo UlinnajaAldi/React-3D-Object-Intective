a0edc352241e32898e2a9ab86e9c6fa738457833
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-24 23:28:23 +0700

    feat: add detail page & use map in route

--- src/App.js
+++ src/App.js
@@ -6,18 +6,28 @@ import { Loading } from "./components/loading/Loading";
 const LandingPage = lazy(() => import("./pages/LandingPage"));
 const ItemsPage = lazy(() => import("./pages/ItemsPage"));
 const Introduce = lazy(() => import("./pages/Introduce"));
+const Detail = lazy(() => import("./pages/Detail"));
 const UnderContruction = lazy(() => import("./pages/UnderContruction"));
 
-export const App = () => {
+export const App = (props) => {
+  const routes = [
+    { path: "/", element: <LandingPage /> },
+    { path: "/items-list", element: <ItemsPage /> },
+    { path: "/introduce", element: <Introduce /> },
+    {
+      path: "/detail/:name",
+      element: <Detail />,
+    },
+    { path: "*", element: <UnderContruction /> },
+  ];
   return (
     <>
       <Navbar />
       <Suspense fallback={<Loading />}>
         <Routes>
-          <Route path="/*" element={<UnderContruction />} />
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/items-list" element={<ItemsPage />} />
-          <Route path="/introduce" element={<Introduce />} />
+          {routes.map((route) => (
+            <Route key={route.path} path={route.path} element={route.element} />
+          ))}
         </Routes>
       </Suspense>
     </>
