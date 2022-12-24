7cd63939b45b7e380d5bd0ece3108fc70f9f1831
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-24 23:30:39 +0700

    feat: add dummy item datas

---
+++ src/utils/items.js
@@ -0,0 +1,58 @@
+const items = [
+  {
+    id: 1,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 2,
+    image: "https://picsum.photos/seed/picsum/400/500",
+    name: "Switch 2",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 3,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 3",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 4,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 4",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 5,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 5",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 6,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 6",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 7,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 7",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 8,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 8",
+    desc: "The switch is like in general and can be clicked",
+  },
+  {
+    id: 9,
+    image: "https://picsum.photos/seed/picsum/400/400",
+    name: "Switch 9",
+    desc: "The switch is like in general and can be clicked",
+  },
+];
+
+export default items;
