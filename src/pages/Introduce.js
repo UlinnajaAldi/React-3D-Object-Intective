6ad4de01e213f0cbe1e9a1855355b043bb69f9a4
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-24 23:35:12 +0700

    feat: change to dummy introduce page

--- src/pages/Introduce.js
+++ src/pages/Introduce.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Toner from "../assets/objects/Toner";
 
 const Introduce = () => {
   return (
     <>
       <div className="flex items-center justify-center">
-        <Toner />
+        <h1 className="text-3xl font-semibold">How to Use?</h1>
       </div>
     </>
   );
