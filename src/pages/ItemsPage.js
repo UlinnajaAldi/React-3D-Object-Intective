19a6abc89ed99607133950f3814349bf6d38c459
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-24 23:36:59 +0700

    feat: add map, showmore/less function

--- src/pages/ItemsPage.js
+++ src/pages/ItemsPage.js
@@ -1,14 +1,99 @@
-import React from "react";
-import Switch from "../assets/objects/Switch";
+import React, { useState } from "react";
+import Card from "../components/card/Card";
+import items from "../utils/items";
 
-const ItemsPage = () => {
+const ItemsPage = (props) => {
+  const [objectItems, setObjectItems] = useState(items.slice(0, 3));
+
+  const shouldShowOrLess = () => objectItems.length < items.length;
+
+  const toggleShowMoreLess = () => {
+    if (shouldShowOrLess()) {
+      setObjectItems((prevObjectItems) => [
+        ...prevObjectItems,
+        ...items.slice(prevObjectItems.length, prevObjectItems.length + 3),
+      ]);
+    } else {
+      setObjectItems(items.slice(0, 3));
+    }
+  };
+
   return (
     <>
-      <div className="h-[80vh]">
-        <Switch />
+      <div className="grid grid-cols-3 gap-6 place-content-center mx-32 my-6">
+        {objectItems.map((item) => (
+          <Card
+            key={item.id}
+            image={item.image}
+            name={item.name}
+            desc={item.desc}
+          />
+        ))}
+      </div>
+      <div className="flex justify-center p-10">
+        <button
+          className="bg-rose-400 hover:bg-rose-500 text-white font-semibold py-2 px-4 rounded-md"
+          onClick={toggleShowMoreLess}
+        >
+          {shouldShowOrLess() ? "Show More ✦" : "Show Less ✦"}
+        </button>
       </div>
     </>
   );
 };
 
 export default ItemsPage;
+
+// import React, { useState } from "react";
+// import Card from "../components/card/Card";
+// import items from "../utils/items";
+
+// const ItemsPage = (props) => {
+//   const [objectItems, setObjectItems] = useState(items.slice(0, 3));
+//   const [isLoading, setIsLoading] = useState(false); // tambahkan variabel isLoading
+
+//   const shouldShowOrLess = () => objectItems.length < items.length;
+
+//   const toggleShowMoreLess = () => {
+//     setIsLoading(true); // ubah isLoading menjadi true
+
+//     // simulasikan loading selama 2 detik
+//     setTimeout(() => {
+//       if (shouldShowOrLess()) {
+//         setObjectItems((prevObjectItems) => [
+//           ...prevObjectItems,
+//           ...items.slice(prevObjectItems.length, prevObjectItems.length + 3),
+//         ]);
+//       } else {
+//         setObjectItems(items.slice(0, 3));
+//       }
+
+//       setIsLoading(false); // ubah isLoading menjadi false
+//     }, 2000);
+//   };
+
+//   return (
+//     <>
+//       <div className="grid grid-cols-3 gap-6 place-content-center mx-32 my-6">
+//         {objectItems.map((item) => (
+//           <Card
+//             key={item.id}
+//             image={item.image}
+//             name={item.name}
+//             desc={item.desc}
+//           />
+//         ))}
+//       </div>
+//       <div className="flex justify-center p-10">
+//         <button
+//           className="bg-rose-400 hover:bg-rose-500 text-white font-semibold py-2 px-4 rounded-md"
+//           onClick={toggleShowMoreLess}
+//         >
+//           {isLoading ? "Loading..." : shouldShowOrLess() ? "Show More ✦" : "Show Less ✦"}
+//         </button>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default ItemsPage;
