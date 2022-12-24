568cf4dfb5cc642a4fd084f34c4d8f6d75ef8d00
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-24 23:35:44 +0700

    refactor: change class to className

--- src/pages/UnderContruction.js
+++ src/pages/UnderContruction.js
@@ -2,16 +2,16 @@ import React from "react";
 
 const UnderContruction = () => {
   return (
-    <div class="flex items-center justify-center mt-20">
-      <div class="flex flex-col items-center justify-center max-w-2xl">
+    <div className="flex items-center justify-center mt-20">
+      <div className="flex flex-col items-center justify-center max-w-2xl">
         <img
           src="https://cdn.pixabay.com/photo/2017/06/20/08/12/maintenance-2422173__340.png"
           alt="tailwindcss maintenance"
         />
-        <h1 class="mb-3 text-3xl font-extrabold text-center text-secondary">
+        <h1 className="mb-3 text-3xl font-extrabold text-center text-secondary">
           Under Maintenance
         </h1>
-        <p class="text-center text-gray-600">
+        <p className="text-center text-gray-600">
           The Page you are looking for is currently under maintenance and will
           be back soon :))
         </p>
