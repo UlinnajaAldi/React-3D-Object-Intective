03bd5304abde60734927d7737a293e223f279ca7
Ulinnaja Aldi <ulinnajaaldi24@gmail.com>
2022-12-26 20:25:21 +0700

    feat: add useMemo and get id from items

--- src/pages/DetailPage.js
+++ src/pages/DetailPage.js
@@ -1,27 +1,32 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { useParams } from "react-router-dom";
 import Detail from "../components/detail/Detail";
 import itemsModule from "../utils/items";
+import Toner from "../assets/objects/Toner";
+import Switch from "../assets/objects/Switch";
 
 const DetailPage = (props) => {
   const [item, setItem] = useState(null);
   const { id } = useParams();
-
   const getItem = itemsModule.getItem;
 
+  const itemData = useMemo(() => getItem(Number(id)), [id, getItem]);
+
   useEffect(() => {
-    setItem(getItem(Number(id)));
-  }, [id, getItem]);
+    setItem(itemData);
+  }, [itemData]);
 
   if (item === null) {
     return <p>Kosong</p>;
   }
 
-  return (
-    <div>
-      <Detail {...item} />
-    </div>
-  );
+  if (id === "1") {
+    return <Toner />;
+  } else if (id === "2") {
+    return <Switch />;
+  } else {
+    return <Detail {...item} />;
+  }
 };
 
 export default DetailPage;
